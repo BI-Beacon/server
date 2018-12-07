8237a0ad0ddc2bfd3c41e91a24ac3696afca7500
Olof Bjarnason <olof.bjarnason@gmail.com>
2018-12-07 09:17:58 +0100

    announcing open source server date

---
+++ src/README.md
@@ -0,0 +1,4 @@
+THE SERVER WILL BE MADE OPEN SOURCE 1ST OF MARCH 2019!
+
+(We need to clean it up a little first haha!)
+
--- src/state_server.py
+++
@@ -1,6 +0,0 @@
-class StateServer(object):
-    def __init__(self, db):
-        self.db = db
-
-    def __getitem__(self, name):
-        return self.db[name]
