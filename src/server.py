86fac2c440c88f53fb5467497c0d471cec22b981
Olof Bjarnason <olof.bjarnason@gmail.com>
2019-05-31 16:09:09 +0200

    wip v0.1 server

--- src/server.py
+++ src/server.py
@@ -1,1 +1,30 @@
-print("hello world")
+import sys
+
+from flask import Flask
+
+
+VERSION = 0.1
+
+
+def run():
+
+    if len(sys.argv) != 2:
+        print("Usage: server.py <port>")
+        print(" port: REST API requests will be available at this port.")
+        return
+
+    port = int(sys.argv[1])
+
+    print("BI-Beacon version {version} starting.".format(version=VERSION))
+    print("Using port: {port}".format(port=port))
+
+    flask_app = Flask(__name__)
+    flask_app.run(host='0.0.0.0',
+                  port=port,
+                  debug=False,
+                  threaded=True)
+    print("Server stopped.")
+
+
+if __name__ == '__main__':
+    run()
--- tst/test_server.py
+++ tst/test_server.py
@@ -1,5 +1,6 @@
 import unittest
 from pprint import pformat
+import time
 import subprocess
 
 import approvaltests
@@ -7,16 +8,72 @@ from approvaltests.combination_approvals import verify_all_combinations
 from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffReporterFactory
 
 from context import server
+import reuseaddr_hack
+from io import StringIO
 
 
-def get_server_stdout_for_inputs(port, request_list):
+def get_process_output_for_inputs(port, request_list):
     cmdline = [
-        # '/bin/ls', '/'
-        'python', 'src/server.py', '80'
+        'python', 'src/server.py'
     ]
-    print(cmdline)
-    result = subprocess.run(cmdline, stdout=subprocess.PIPE)
-    return result.stdout.decode('ascii').splitlines()
+    if port:
+        cmdline.append(str(port))
+
+    stdout = open('/tmp/output.txt', 'w')
+    stderr = open('/tmp/error.txt', 'w')
+    process = subprocess.Popen(
+        cmdline,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    time.sleep(0.4)
+    process.terminate()
+    sout, serr = process.communicate(timeout=2)
+
+    # process.wait(timeout=2)
+
+    # stdout.close()
+    # stderr.close()
+
+    # with open('/tmp/output.txt') as f:
+    #     sout = f.read()
+    # with open('/tmp/error.txt') as f:
+    #     serr = f.read()
+import unittest
+from pprint import pformat
+import time
+import subprocess
+
+import approvaltests
+from approvaltests.combination_approvals import verify_all_combinations
+from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffReporterFactory
+
+from context import server
+import reuseaddr_hack
+from io import StringIO
+
+
+def get_process_output_for_inputs(port, request_list):
+    cmdline = [
+        'python', 'src/server.py'
+    ]
+    if port:
+        cmdline.append(str(port))
+
+    process = subprocess.Popen(
+        cmdline,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    time.sleep(0.4)
+    process.terminate()
+    sout, serr = process.communicate(timeout=2)
+
+    return {
+        'stdout': sout.decode('ascii'),
+        'stderr': serr.decode('ascii'),
+        'returncode': process.returncode
+    }
 
 
 class TestServer(unittest.TestCase):
@@ -30,7 +87,7 @@ class TestServer(unittest.TestCase):
 
     def test_server_process(self):
         inputs = [
-            [80, 8888],
+            [None, 8890],
             [
                 [
                     ('GET', 'test'),
@@ -40,7 +97,7 @@ class TestServer(unittest.TestCase):
             ]
         ]
         verify_all_combinations(
-            get_server_stdout_for_inputs,
+            get_process_output_for_inputs,
             inputs,
             reporter=self.reporter,
             formatter=result_formatter)
@@ -50,6 +107,50 @@ def result_formatter(args, result):
     return '\nGiven: {args}\nGot:\n{result}\n'.format(
         args=pformat(args),
         result=pformat(result, width=60, indent=2)
+    )
+
+
+# if __name__ == '__main__':
+#     unittest.main()
+
+    return {
+        'stdout': sout,
+        'stderr': serr,
+        'returncode': process.returncode
+    }
+
+
+class TestServer(unittest.TestCase):
+
+    def setUp(self):
+        factory = GenericDiffReporterFactory()
+        import os
+        print(os.getcwd())
+        factory.load('tst/reporters.json')
+        self.reporter = factory.get('meld')
+
+    def test_server_process(self):
+        inputs = [
+            [None, 9999],
+            [
+                [
+                    ('GET', 'test'),
+                    ('POST', 'test', {'color': '#ff00ff'}),
+                    ('GET', 'test')
+                ]
+            ]
+        ]
+        verify_all_combinations(
+            get_process_output_for_inputs,
+            inputs,
+            reporter=self.reporter,
+            formatter=result_formatter)
+
+
+def result_formatter(args, result):
+    return '\nGiven: {args}\nGot:\n{result}\n'.format(
+        args=pformat(args),
+        result=pformat(result, width=6000, indent=2)
     )
 
 
